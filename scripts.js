a361954b28c202e5e5fcc06be031da38312dcc36
Adrian Bodin Thorn <bodinthornadrian@gmail.com>
2023-09-28 09:23:52 +0200

    Finished the project

--- scripts.js
+++ scripts.js
@@ -2,33 +2,26 @@
 const USERNAME = "Bella";
 const PASSWORD = "qwe123";
 
-
-//Drar in element som jag kommer behöva manipulera på ett eller annat sätt
 const usernameInput = document.getElementsByClassName("username-input")[0];
 const passwordInput = document.getElementsByClassName("password-input")[0];
 const loginButton = document.getElementsByClassName("mainbutton")[0];
-//Kommer att användas för display:none; när användaren loggat in.
 const heroSection = document.getElementsByClassName("hero-section")[0];
-//Kommer användas för att sätta in errormessage elementet i
 const loginForm = document.getElementsByClassName("input-form")[0];
 
-/*Denna lyssnar efter click på login knappen. Och kollar om användarnamnet och lösenordet 
-stämmer och kallar en funktion annars skickar den ut felmeddelande*/
+if(localStorage.getItem("loggedin")) {
+    logInUser();
+}
+
 loginButton.addEventListener("click", () => {
     if(usernameInput.value === USERNAME && passwordInput.value === PASSWORD) {
         logInUser();
-    }
-    else if(passwordInput.value === "") {
+    } else if(passwordInput.value === "") {
         outputErrorMessage("Du måste fylla i ett lösenord.");
-    }
-    else {
+    } else {
         outputErrorMessage("Inloggning misslyckades. Var vänlig försök igen.");
     }
 });
 
-/* Denna funktionen får man mata in ett meddelande man vill ska sägas upp i 
-felmeddelandet och kollar sedan av ifall det redan finns ett felmeddelande element
-och tar isåfall bort detta om det finns och klistrar in ett nytt. */
 function outputErrorMessage(message) {
 
     const checkErrorMessage = document.getElementById("error-message");
@@ -42,19 +35,12 @@ function outputErrorMessage(message) {
     errorMessage.appendChild(outputText);
     errorMessage.setAttribute("id", "error-message");
     loginForm.appendChild(errorMessage);
-    clearInputs()
-    
-    
-    
-    
-    
+    clearInputs();
 }
 
-/* Denna funktionen gör så att vi kommer in till "nästa sida". Och därifrån lyssnar
-den på logout knappen och skickar oss tillbaka till startsidan om man trycker på den */
 function logInUser() {
     heroSection.style.display = "none";
-    clearInputs()
+    clearInputs();
     //Skapar en ny section och lägger en class på den
     const loggedInSection = document.createElement("section");
     loggedInSection.setAttribute("class", "logged-in-section");
@@ -85,12 +71,6 @@ function logInUser() {
 
 }
 
-if(localStorage.getItem("loggedin")) {
-    logInUser()
-}
-
-//En enkel funktion som rensar input fälten efter olika event. Denna kallar jag på 
-//i andra funktioner.
 function clearInputs() {
     usernameInput.value = "";
     passwordInput.value = "";
